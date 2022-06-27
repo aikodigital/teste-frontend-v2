a09ca299f35e5f8cb84342ce3acca54781db9034
JanainaNogueira <janainanogueira464@gmail.com>
2022-06-27 01:23:42 -0300

    Cria pasta InfoBar e o arquivo EquipamentComponent
    - Pasta usada para agrupar conponents da barra de informações
    - Arquivo onde serão mostrados as informações dos equipamentos

---
+++ src/components/InfoBar/EquipamentComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="infobar__dados">
+    <div>
+      <h2>Nome equipamento</h2>
+      <p>Modelo do equipamento</p>
+      <hr>
+    </div>
+    <div>
+      <h3>Posição atual</h3>
+      <hr>
+      <button>Histórico de posiçoes</button>
+    </div>
+    <div>
+      <h3>Estado do equipamento</h3>
+      <hr>
+      <button>Histórico de estados</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'EquipamentComponent',
+};
+</script>
