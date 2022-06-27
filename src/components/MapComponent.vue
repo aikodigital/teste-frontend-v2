c64259e9ba5125c176d9abb4abc5370f771f5051
JanainaNogueira <janainanogueira464@gmail.com>
2022-06-27 00:18:27 -0300

    Adiciona o mapa do leaflet ao MapComponent
    - Importada a instância onMounted do vue.
    - Criação da camada de bloco tileLayer onde as
      imagens do OpenStreetMap serão renderizadas.

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="map">
-    <h1>This is an about page</h1>
+  <div class="map" id="map">
   </div>
 </template>
 
 <script>
+import leaflet from 'leaflet';
+import { onMounted } from 'vue';
+
 export default {
   name: 'MapComponent',
+  setup() {
+    let map;
+    onMounted(() => {
+      map = leaflet.map('map').setView([51.505, -0.09], 13);
+      leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap',
+      }).addTo(map);
+    });
+  },
 };
 </script>
 
@@ -15,5 +27,7 @@ export default {
     width: 70%;
     height: 100%;
     border-radius: 0 15px 15px 0;
+    overflow :hidden;
+    z-index: 10;
   }
 </style>
