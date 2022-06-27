5cf318dd7b85a0ccd6a8a318c5ec33499ffba7e0
JanainaNogueira <janainanogueira464@gmail.com>
2022-06-27 20:03:23 -0300

    Faz requisição dos dados e mostra para o usuário
    Faz requisição dos dados nas rotas
    - Equipment.json : name
    - EquipmentModel.json : name
    - equipmentState.son : name
    Mostra ao usuário utilizando v-for

--- src/components/InfoBar/EquipamentComponent.vue
+++ src/components/InfoBar/EquipamentComponent.vue
@@ -1,19 +1,25 @@
 <template>
   <section class="infobar__dados">
-    <div>
+    <div class="dados-container">
       <h2>Nome equipamento</h2>
-      <p>Modelo do equipamento</p>
       <hr>
+      <div class="list-info">
+        <p v-for = "(name, index) in equipment" :key="index">{{name.name}}</p>
+      </div>
     </div>
-    <div>
-      <h3>Posição atual</h3>
+    <div class="dados-container">
+      <h3>Modelo do equipamento</h3>
       <hr>
-      <button>Histórico de posiçoes</button>
+      <div class="list-info">
+        <p v-for = "(name, index) in model" :key="index">{{name.name}}</p>
+      </div>
     </div>
-    <div>
+    <div class="dados-container">
       <h3>Estado do equipamento</h3>
       <hr>
-      <button>Histórico de estados</button>
+      <div class="list-info">
+        <p v-for = "(name, index) in state" :key="index">{{name.name}}</p>
+      </div>
     </div>
   </section>
 </template>
@@ -21,5 +27,41 @@
 <script>
 export default {
   name: 'EquipamentComponent',
+  data() {
+    return {
+      model: null,
+      equipment: null,
+      state: null,
+    };
+  },
+  methods: {
+    async getModels() {
+      // retorno dos dados de equipmentModel.json
+      const reqModel = await fetch('http://localhost:3000/model');
+      const dataModel = await reqModel.json();
+      this.model = dataModel;
+      // retorno dos dados de equipment.json
+      const reqEquipment = await fetch('http://localhost:3000/equipment');
+      const dataEquipment = await reqEquipment.json();
+      this.equipment = dataEquipment;
+      // retorno dos dados de equipmentSate.json
+      const reqState = await fetch('http://localhost:3000/state');
+      const dataState = await reqState.json();
+      this.state = dataState;
+    },
+  },
+  mounted() {
+    this.getModels();
+  },
 };
 </script>
+
+<style scoped>
+  .dados-container{
+    height:25%;
+  }
+  .list-info{
+    height: 62%;
+    overflow-y: scroll;
+  }
+</style>
